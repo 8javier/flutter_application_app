85c7ec5bd402f1ea74c7ee19f3d75960ef58c291
Nahir Cavallero <nvignolo@uade.edu.ar>
2023-05-08 14:43:55 -0300

    Pantalla Registro profesional

---
+++ lib/pantallas/registro_profesional.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+
+class RegistroProfesional extends StatelessWidget {
+  bool _acceptedTerms = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 42.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Teléfono',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 42.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Matrícula',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 42.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 42.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              CheckboxListTile(
+                title: Text('Acepto los términos y Condiciones'),
+                value: _acceptedTerms,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    _acceptedTerms = newValue;
+                  });
+                },
+              ),
+              SizedBox(height: 42.0),
+              ElevatedButton(
+                onPressed: () {
+                  
+                },
+                child: Text('Aceptar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
