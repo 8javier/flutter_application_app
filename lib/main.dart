431154a0ca740d5da4947e002c175437b96ea3e7
Jeronimo Juarez Tabares <jerojuareztaba@gmail.com>
2023-05-21 14:14:46 -0300

    cambio en el test
    Cambie como se definia el peso para las opciones en el Test en  caso de que haya un problema

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 //--------------  Paquetes que usa para la conexion a Firebase
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_app/pantallas/EncuestaScreen.dart';
 import 'package:flutter_application_app/pantallas/edit_paciente_page.dart';
 import 'firebase_options.dart';
 //-------------- ------------------------------------------------------
 //  Paginas
 import 'package:flutter_application_app/pantallas/add_paciente_page.dart';
 import 'package:flutter_application_app/pantallas/home_page.dart';
+
+import 'modelos/encuestas/EncuestaDinamica.dart';
+import 'modelos/encuestas/Opcion.dart';
+import 'modelos/encuestas/Pregunta.dart';
 // --------------------------------------------------------
 void main() async {
   // <----Agrega variable[async] // q espera a conectarse a Firebase-Core
-  WidgetsFlutterBinding.ensureInitialized();
+  /*WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     //options: DefaultFirebaseOptions.android, // if you're using windows emulator
     //options: DefaultFirebaseOptions.ios, // para plataforma ios
     options: DefaultFirebaseOptions.currentPlatform, // no usen la opcion windows solo web o android
      //options: DefaultFirebaseOptions.web, 
-  );
+  );*/
 
-  runApp(const MyApp()); // <-----Arranca la App
+  //funcion para crear un encuesta con 10 preguntas random y 3 opciones en cada pregunta tambien random
+  EncuestaDinamica generarEncuesta() {
+    List<Pregunta> preguntas = [];
+    Random random = Random();
+
+    for (int i = 1; i <= 10; i++) {
+      List<Opcion> opciones = [];
+
+      for (int j = 1; j <= 3; j++) {
+        int peso = random.nextInt(3) + 1;
+        opciones.add(Opcion(j, peso, 'Opción $j'));
+      }
+
+      int pesoPregunta = random.nextInt(10) + 1;
+      preguntas.add(Pregunta(i, pesoPregunta, 'Pregunta $i', opciones, false));
+    }
+
+    return EncuestaDinamica(1, preguntas, 0);
+  }
+
+  EncuestaDinamica encuesta = generarEncuesta();
+
+  runApp(MyApp(encuesta: encuesta,));
+
+
+  //runApp(const MyApp()); // <-----Arranca la App
 }
-
-// -----------------------------------------------------------
 class MyApp extends StatelessWidget {
+  final EncuestaDinamica encuesta;
+
+  const MyApp({super.key, required this.encuesta});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: EncuestaScreen(encuesta: encuesta),
+    );
+  }
+}
+// -----------------------------------------------------------
+/*class MyApp extends StatelessWidget {
   const MyApp({super.key});
 // --------------ROOT APP--------------------------------------
 // This widget is the root of your application.
@@ -40,5 +83,5 @@ class MyApp extends StatelessWidget {
         },
         
         );
-  }
-}
+  }*/
+
