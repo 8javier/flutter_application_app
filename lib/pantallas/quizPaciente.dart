8c0da9f1702001e45e8287fdd962c7686aa867de
aldael <luchii.aldaz@gmail.com>
2023-06-03 22:13:34 -0300

    [pantalla para cuestionarios(copia de preguntas)]

--- lib/pantallas/quizPaciente.dart
+++ lib/pantallas/quizPaciente.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../reciclar/textfield.dart';
+
 class QuizPaciente extends StatefulWidget {
   const QuizPaciente({super.key});
 
@@ -12,7 +14,13 @@ class _QuizPacienteState extends State<QuizPaciente> {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        Text("Quiz")
+        Padding(padding: const EdgeInsets.all(20),
+        child: Text("Question",
+        style: TextStyle(fontSize: 25),),
+        ),
+        Padding(padding: const EdgeInsets.all(20),
+        child:texto(20) ,),
+        IconButton(onPressed: (){}, icon: Icon(Icons.send))
       ],
     );
   }
--- lib/pantallas/recomendaciones.dart
+++ lib/pantallas/recomendaciones.dart
@@ -1,65 +1,68 @@
 import 'package:flutter/material.dart';
 
-
-class Recommend extends StatelessWidget{
-  const Recommend({Key? key}) : super (key: key);
+class Recommend extends StatelessWidget {
+  const Recommend({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Recomendaciones"),
-      ),
-      body: ListView(
-        children: <Widget>[
-          Row(
-          children: [
-            InkWell(
-              child:
-            Container(
-              margin:
-                  const EdgeInsets.only(top: 5, bottom: 5, right: 5, left: 10),
-              height: 75,
-              width: 190,
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Color(0x11838282),
-                  borderRadius: BorderRadius.circular(10)),
-              child: Text("Estado físico"),
-            ),
-            onTap: () {
-              showDialog(
-            context: context, 
-            builder: (_)=> new AlertDialog(
-              title: Text("[Proximamente...]"),
-            ));
-            },
-            ),
-            InkWell(
-              child: Container(
-              margin:
-                  const EdgeInsets.only(top: 5, bottom: 5, right: 10, left: 5),
-              height: 75,
-              width: 190,
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Color(0x11838282),
-                  borderRadius: BorderRadius.circular(10)),
-              child: Text("Links"),
-            ),
-            onTap: () {
-              showDialog(
-            context: context, 
-            builder: (_)=> new AlertDialog(
-              title: Text("[Proximamente...]"),
-            ));
-            },
-            )
-            
-          ],
+        appBar: AppBar(
+          title: Text("Recomendaciones"),
         ),
-          Text("Escribir recomendaciones y sus checks"),
-        ],)
-    );
+        body: ListView(
+          children: <Widget>[
+            Row(
+              children: [
+                InkWell(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        top: 10, bottom: 5, right: 5, left: 10),
+                    height: 75,
+                    width: 190,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color(0x11838282),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text("Estado físico"),
+                  ),
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) => new AlertDialog(
+                              title: Text("[Proximamente...]"),
+                            ));
+                  },
+                ),
+                InkWell(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        top: 10, bottom: 5, right: 10, left: 5),
+                    height: 75,
+                    width: 190,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color(0x11838282),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text("Links"),
+                  ),
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (_) => new AlertDialog(
+                              title: Text("[Proximamente...]"),
+                            ));
+                  },
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "Escriba aquí sus recomendaciones y sus checks",
+                style: TextStyle(fontSize: 25),
+              ),
+            ),
+          ],
+        ));
   }
 }
