5569d464287e83c3698fa094dcb867070f26f16e
Jeronimo Juarez Tabares <jerojuareztaba@gmail.com>
2023-07-02 18:26:17 -0300

    Update listaPacientesPage.dart

--- lib/pantallas/listaPacientesPage.dart
+++ lib/pantallas/listaPacientesPage.dart
@@ -1,15 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:excel/excel.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+import '../modelos/encuestas/EncuestasDinamica.dart';
+import '../modelos/encuestas/Opcion.dart';
+import '../modelos/encuestas/Pregunta.dart';
+import '../modelos/encuestas/preguntaDinamica.dart';
 import '../modelos/paciente_datos.dart';
-import '../modelos/provider/profesional_provider.dart';
+import '../modelos/provider/ProfesionalProvider.dart';
+import '../servicios/FirebaseServiceEncuestas.dart';
+import 'dart:io';
+
 class ListaPacientePage extends StatefulWidget {
   const ListaPacientePage({super.key});
   @override
   State<ListaPacientePage> createState() => _ListaPacientePageState();
 }
 class _ListaPacientePageState extends State<ListaPacientePage> {
- @override
+  @override
   void initState() {
     super.initState();
     // Carga los pacientes del profesional al iniciar la página
@@ -21,63 +31,235 @@ class _ListaPacientePageState extends State<ListaPacientePage> {
   Widget build(BuildContext context) {
     final profesionalProvider = Provider.of<ProfesionalProvider>(context);
     final List<Paciente> pacientes = profesionalProvider.pacientesLista;
-      return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Pacientes'),
       ),
       body: ListView.builder(
         itemCount: pacientes.length,
         itemBuilder: (context, index) {
-          Paciente paciente = pacientes[index]; // Carga para mostrar la info en pantalla
-            return ListTile(
+          final paciente = pacientes[index]; // Carga para mostrar la info en pantalla
+          return ListTile(
             title: Text(paciente.nombre ?? ''),
             subtitle: Text(paciente.apellido ?? ''),
-        trailing: Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      IconButton(
-        icon: const Icon(Icons.message),
-        onPressed: () {
-          enviarMensaje(paciente); // Llama a la función para enviar el mensaje al paciente
-        },
-      ),
-      IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: () {
-          eliminarPaciente(paciente); // Llama a la función para eliminar el paciente
-        },
-      ),
-    ],
-  ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.message),
+                  onPressed: () {
+                    cargarPreguntaDesdeExcel(); // Llama a la función para enviar el mensaje al paciente
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.message),
+                  onPressed: () {
+                    cargarEncuestaDesdeExcel(); // Llama a la función para enviar el mensaje al paciente
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.message),
+                  onPressed: () {
+                    enviarMensaje(paciente); // Llama a la función para enviar el mensaje al paciente
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    eliminarPaciente(paciente); // Llama a la función para eliminar el paciente
+                  },
+                ),
+              ],
+            ),
           );
         },
       ),
     );
   }
 
-Future<void> enviarMensaje(Paciente paciente) async {
-  final profesionalProvider = Provider.of<ProfesionalProvider>(context, listen: false);
-  // Captura la información del paciente seleccionado
-  final pacienteId = paciente.id;
-  final collectionReference = FirebaseFirestore.instance.collection("paciente/$pacienteId");
-  // Verifica si la subcolección "mensajeProfesional" existe
-  final subcollectionReference = collectionReference.doc(pacienteId).collection("mensajeProfesional");
-  final snapshot = await subcollectionReference.get();
-  if (snapshot.docs.isEmpty) {
-    print('La colección "mensajeProfesional" no existe. Creando...');
-    await subcollectionReference.doc().set({});
-    print('La colección "mensajeProfesional" ha sido creada con éxito.');
-  } else {
-    print('La colección "mensajeProfesional" ya existe.');
+  Future<void> enviarMensaje(Paciente paciente) async {
+    final profesionalProvider = Provider.of<ProfesionalProvider>(context, listen: false);
+    // Captura la información del paciente seleccionado
+    final pacienteId = paciente.id;
+    final collectionReference = FirebaseFirestore.instance.collection("paciente/$pacienteId");
+    // Verifica si la subcolección "mensajeProfesional" existe
+    final subcollectionReference = collectionReference.doc(pacienteId).collection("mensajeProfesional");
+    final snapshot = await subcollectionReference.get();
+    if (snapshot.docs.isEmpty) {
+      print('La colección "mensajeProfesional" no existe. Creando...');
+      await subcollectionReference.doc().set({});
+      print('La colección "mensajeProfesional" ha sido creada con éxito.');
+    } else {
+      print('La colección "mensajeProfesional" ya existe.');
+    }
+    // Agrega el mensaje al documento en la subcolección
+    await subcollectionReference.add({
+      "mensaje": "Este es un mensaje para el paciente",
+      "fecha": DateTime.now(),
+    });
   }
-  // Agrega el mensaje al documento en la subcolección
-  await subcollectionReference.add({
-    "mensaje": "Este es un mensaje para el paciente",
-    "fecha": DateTime.now(),
-  });
-}
   void eliminarPaciente(Paciente paciente) {// funcion que elimina de la DB del peofesional
     final profesionalProvider = Provider.of<ProfesionalProvider>(context, listen: false);
     profesionalProvider.eliminarPaciente(paciente);
+  }
+
+  Future<bool> cargarEncuestaDesdeExcel() async {
+    // Abre el selector de archivos para seleccionar un archivo Excel
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx'],
+    );
+
+    var uuid = Uuid();
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+
+      // Carga el archivo Excel utilizando la dependencia 'excel'
+      var bytes = File(file.path!).readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+
+      // Obtiene el nombre del archivo como el nombre de la encuesta
+      String nombreEncuesta = file.name;
+
+      // Crea un objeto de EncuestaDinamica
+      EncuestaDinamica encuesta = EncuestaDinamica(
+        id: uuid.v4(),
+        nombre:nombreEncuesta,
+        preguntas: [],
+        pesoTotal: 0,
+      );
+
+      // Obtiene la hoja de trabajo principal del libro de Excel
+      var sheet = excel.tables[excel.tables.keys.first];
+
+      // Obtiene el número total de filas en la hoja
+      var rowCount = sheet?.rows.length ?? 0;
+
+      // Itera sobre las filas del archivo Excel
+      for (var i = 0; i < rowCount; i += 2) {
+        var preguntaRow = sheet!.rows[i];
+        var opcionesRow = sheet.rows[i + 1];
+
+        // Verifica si la fila de preguntas y la fila de opciones tienen datos
+        if (preguntaRow[0]?.value != null && opcionesRow[0]?.value != null) {
+          var preguntaTexto = preguntaRow[0]?.value.toString();
+          var preguntaPeso = double.parse(preguntaRow[1]?.value);
+          var pregunraFinal = preguntaRow[2]?.value.toString();
+          var esFinal = false;
+
+          if (pregunraFinal == 'X' || pregunraFinal == 'x') {
+            var esFinal = true;
+          }
+
+          // Crea una nueva pregunta
+          Pregunta pregunta = Pregunta(
+            id: uuid.v4(),
+            peso: preguntaPeso as int,
+            texto: preguntaTexto!,
+            opciones: [],
+            esFinal: esFinal,
+          );
+
+          // Itera sobre las columnas de opciones y pesos
+          for (var j = 0; j < opcionesRow.length; j += 1) {
+            if (opcionesRow[j]?.value != null && opcionesRow[j + 1]?.value != null) {
+              var opcionTexto = opcionesRow[j]?.value.toString();
+              var opcionPeso = double.parse(opcionesRow[j + 1]?.value);
+
+              // Crea una nueva opción y la agrega a la pregunta
+              Opcion opcion = Opcion(
+                id: uuid.v4(),
+                peso: opcionPeso as int,
+                texto: opcionTexto!,
+
+              );
+              pregunta.opciones.add(opcion);
+            }
+          }
+
+          // Agrega la pregunta a la encuesta
+          encuesta.preguntas.add(pregunta);
+        }
+      }
+
+      // Guarda la encuesta en Firebase
+      EncuestaService encuestaService = EncuestaService();
+      await encuestaService.guardarEncuestaDinamica(encuesta);
+
+      return true;
+    } else {
+      // El usuario canceló la selección del archivo
+      return false;
+    }
+  }
+
+  Future<bool> cargarPreguntaDesdeExcel() async {
+    // Abre el selector de archivos para seleccionar un archivo Excel
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx'],
+    );
+
+    const uuid = Uuid();
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+
+      // Carga el archivo Excel utilizando la dependencia 'excel'
+      var bytes = File(file.path!).readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+
+      // Obtiene la hoja de trabajo principal del libro de Excel
+      var sheet = excel.tables[excel.tables.keys.first];
+
+      // Obtiene el número total de filas en la hoja
+      var rowCount = sheet?.rows.length ?? 0;
+
+      // Itera sobre las filas del archivo Excel
+      for (var i = 0; i < rowCount; i += 2) {
+        var preguntaRow = sheet!.rows[i];
+        var opcionesRow = sheet.rows[i + 1];
+
+        // Verifica si la fila de preguntas y la fila de opciones tienen datos
+        if (preguntaRow[0]?.value != null && opcionesRow[0]?.value != null) {
+          var preguntaTexto = preguntaRow[0]?.value.toString();
+          var preguntaPeso = double.parse(preguntaRow[1]?.value);
+
+
+          // Crea una nueva pregunta
+          PreguntaDinamica pregunta = PreguntaDinamica(
+            id: uuid.v4(),
+            texto: preguntaTexto!,
+            opciones: [],
+          );
+
+          // Itera sobre las columnas de opciones y pesos
+          for (var j = 0; j < opcionesRow.length; j += 1) {
+            if (opcionesRow[j]?.value != null && opcionesRow[j + 1]?.value != null) {
+              var opcionTexto = opcionesRow[j]?.value.toString();
+              var opcionPeso = double.parse(opcionesRow[j + 1]?.value);
+
+              // Crea una nueva opción y la agrega a la pregunta
+              Opcion opcion = Opcion(
+                id: uuid.v4(),
+                peso: opcionPeso as int,
+                texto: opcionTexto!,
+
+              );
+              pregunta.opciones.add(opcion);
+            }
+          }
+
+        }
+      }
+
+      // Calcula el peso total de la encuesta
+
+      return true;
+    } else {
+      // El usuario canceló la selección del archivo
+      return false;
+    }
   }
 }
