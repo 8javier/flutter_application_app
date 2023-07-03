e286e9a6f3b3ebd4e66ce5ec1892521b8ca7cb19
aldael <luchii.aldaz@gmail.com>
2023-07-03 03:16:49 -0300

    [pequeños cambios visuales]
    [Eliminacion de barra baja en home_inicio, creacion de la barrabaja en lista de pacientes, implementacion de los 3 botones en lista de pacientes en la barrabaja]

---
+++ lib/reciclar/barrabaja.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class DemoBottomAppBar extends StatefulWidget {
+  const DemoBottomAppBar({
+    super.key
+  });
+
+  @override
+  State<DemoBottomAppBar> createState() => DemoBottomAppBarState();
+
+  static final List<FloatingActionButtonLocation> centerLocations =
+      <FloatingActionButtonLocation>[
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.centerFloat,
+  ];
+}
+
+class DemoBottomAppBarState extends State<DemoBottomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.blue,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              tooltip: 'lista de profesionales',
+              icon: const Icon(Icons.list_alt_sharp),
+              onPressed: () async {
+          await Navigator.pushNamed(context, '/lisProfesional');
+          setState(() {});
+        },
+            ),
+            
+            IconButton(
+              tooltip: 'añadir paciente',
+              icon: const Icon(Icons.accessibility_new),
+              onPressed: () async {
+          await Navigator.pushNamed(context, '/addPaciente');
+
+          setState(() {});
+        },
+            ),
+            IconButton(
+              tooltip: 'añadir profesional',
+              icon: const Icon(Icons.add),
+              onPressed: () async {
+          //<-agrega un async por que se interactua con la base de datos
+          await Navigator.pushNamed(context,
+              '/addProfesional'); //<-agrega await para que pause la sincronia con la base y hace una actualizacion de los datos que hay en la base aca va '/add' de paciente
+
+          setState(
+              () {}); //<-- actualiza la info del Home_page para que muestre los datos da la base de datos
+        },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
